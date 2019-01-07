7cccfefec905d99a97de44522c1322077ba5f933
AlvinMugambi <ngurual123@gmail.com>
2019-01-08 02:46:49 +0300

    [chore #163033755]register blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 """Creates the app instance and returns the app """
 
 from flask import Flask
+from app.api.v1.views.meetup_views import version1 as meetups
 
 def create_app():
     """Creates the app instance and returns the app """
     app = Flask(__name__)
-
+    app.register_blueprint(meetups)
     return app
