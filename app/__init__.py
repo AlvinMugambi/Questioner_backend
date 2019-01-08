b1bee4b587466ee7c2d2f6e618f0afd28aa70df0
AlvinMugambi <ngurual123@gmail.com>
2019-01-08 15:56:32 +0300

    [chore #163033755]Change app config to uppercase to conform to python

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ Creates the app instance and returns the app
 from flask import Flask, Blueprint
 
 from app.api.v1.views.meetup_views import version1 as meetups
-from config import app_config
+from config import APP_CONFIG
 
 
 def create_app(app_environment):
@@ -15,7 +15,7 @@ def create_app(app_environment):
     Creates the app instance and returns the app
     """
     app = Flask(__name__)
-    app.config.from_object(app_config[app_environment])
+    app.config.from_object(APP_CONFIG[app_environment])
     app.register_blueprint(meetups)
 
     return app
