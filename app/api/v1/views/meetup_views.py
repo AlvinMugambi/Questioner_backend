542c018b1a5b99c33d5860b068dbb784d3ced2fe
AlvinMugambi <ngurual123@gmail.com>
2019-01-08 19:34:42 +0300

    [chore #163033755]Improve code quality

--- app/api/v1/views/meetup_views.py
+++ app/api/v1/views/meetup_views.py
@@ -1,32 +1,36 @@
-from flask import Flask, jsonify, request
-import datetime
+"""The meetup routes"""
 
-from app.api.v1.models.models import meetups, Meetup
+from flask import Flask, jsonify, request
+
+from app.api.v1.models.models import Meetup
 from app.api.v1 import version1
 
 @version1.route("/meetups", methods=['POST'])
 def create_meetup():
+    """
+    The POST method for the meetups routes that allows a user to create a meetup
+    """
     try:
         topic = request.get_json()['topic']
         meetup_date = request.get_json()['meetup_date']
         location = request.get_json()['location']
         images = request.get_json()['images']
-        tags= request.get_json()['tags']
+        tags = request.get_json()['tags']
 
     except:
-        return jsonify({'Key Error': 'Check your json keys. Should be topic, meetup_date, location, images and tags'})
+        return jsonify({'status':400,
+                        'error': 'Check your json keys. Should be topic, meetup_date, location, images and tags'}), 400
 
     if not topic:
-        return jsonify({'Error':'topic fields is required'})
-
+        return jsonify({'status':400, 'error':'topic field is required'}), 400
     if not meetup_date:
-        return jsonify({'Error':'meetup_date fields is required'})
+        return jsonify({'status':400, 'error':'meetup_date field is required'}), 400
 
     if not location:
-        return jsonify({'Error':'location fields is required'})
+        return jsonify({'status':400, 'error':'location field is required'}), 400
 
     if not tags:
-        return jsonify({'Error':'tags fields is required'})
+        return jsonify({'status':400, 'error':'tags field is required'}), 400
 
     meetup = Meetup(
         topic=topic,
@@ -37,4 +41,8 @@ def create_meetup():
     )
     meetup.create_meetup()
 
-    return jsonify({"status": 201, "data": [{"topic": topic, "location": location, "meetup_date": meetup_date, "tags": tags}]}), 201
+    return jsonify({"status": 201,
+                    "data": [{"topic": topic,
+                              "location": location,
+                              "meetup_date": meetup_date,
+                              "tags": tags}]}), 201
