d59a0bcc511540b4a0ef6a030620faba45d48230
AlvinMugambi <ngurual123@gmail.com>
2019-01-08 15:30:31 +0300

    [chore #163033755]Add app configuration to the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 """Creates the app instance and returns the app """
 
-from flask import Flask, Blueprint
-from app.api.v1.views.meetup_views import version1 as meetups
 
-def create_app():
+# local imports
+from flask import Flask, Blueprint
+
+from app.api.v1.views.meetup_views import version1 as meetups
+from config import app_config
+
+
+def create_app(app_environment):
     """Creates the app instance and returns the app """
     app = Flask(__name__)
+    app.config.from_object(app_config[app_environment])
     app.register_blueprint(meetups)
 
     return app
