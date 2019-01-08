a1b8b19693128f5207d626aca52ad522fd82b520
AlvinMugambi <ngurual123@gmail.com>
2019-01-08 15:58:01 +0300

    [feature #163033755]Adds the create meetup model method to the Meetups class

--- app/api/v1/models/models.py
+++ app/api/v1/models/models.py
@@ -1,11 +1,20 @@
-from datetime import datetime
+"""
+The meetups models
+"""
 
+from datetime import datetime
 
 meetups = []
 
-class Meetup():
 
-    def __init__(self, id, topic, meetup_date, location, images, tags):
+class Meetup:
+    """
+    The Meetup class that contains the meetup models
+    """
+    def __init__(self, topic, meetup_date, location, images, tags):
+        """
+        The initialization of the Meetup class that defines its variables
+        """
         self.id = len(meetups)+1
         self.topic = topic
         self.meetup_date = meetup_date
@@ -13,3 +22,26 @@ class Meetup():
         self.images = images
         self.tags = tags
         self.created_at = datetime.now()
+
+    def create_meetup(self):
+        """
+        saves new meetup to store
+        """
+        meetups.append(self)
+        print(">>>>>>>>>>>\n", self.id, self.images, self.created_at)
+        print("STORE ", meetups)
+
+    @staticmethod
+    def to_json(meetup):
+        """
+        format meetup object to a readable dictionary
+        """
+        return {
+            "id": meetup.id,
+            "topic": meetup.topic,
+            "meetup_date": meetup.meetup_date,
+            "location": meetup.location,
+            "images": meetup.images,
+            "tags": meetup.tags,
+            "created_at": meetup.created_at
+        }
