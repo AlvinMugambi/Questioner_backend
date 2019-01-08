09c25da1e53ff77f76f1263a3b5bd429c8976a38
AlvinMugambi <ngurual123@gmail.com>
2019-01-08 17:06:50 +0300

    [chore #163033755]change the dict name to conform to the config

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,18 @@
 """
-Creates the app instance and returns the app
+Creates the app function and returns the app
 """
-
 
 # local imports
 from flask import Flask, Blueprint
 
 from app.api.v1.views.meetup_views import version1 as meetups
-from config import APP_CONFIG
+from config import app_config
 
 
 def create_app(app_environment):
-    """
-    Creates the app instance and returns the app
-    """
+    """Creates the app instance and returns the app """
     app = Flask(__name__)
-    app.config.from_object(APP_CONFIG[app_environment])
+    app.config.from_object(app_config[app_environment])
     app.register_blueprint(meetups)
 
     return app
