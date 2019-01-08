105db603a270b1c7636916bc69ea32ef96939932
AlvinMugambi <ngurual123@gmail.com>
2019-01-09 00:14:37 +0300

    [start #163034192]add failing test for the get all meetups endpoint

--- tests/v1/test_meetups.py
+++ tests/v1/test_meetups.py
@@ -14,16 +14,38 @@ class MeetupsBaseTest(unittest.TestCase):
     """
 
     def setUp(self):
-        APP_ENV = os.getenv("TESTING_ENV")
-        self.app = create_app(APP_ENV)
+
+        self.app = create_app("testing")
         self.client = self.app.test_client()
 
-        self.post_meetup = {"topic":"Miraa",
-                            "meetup_date":"30/01/1990",
-                            "location":"Meru",
-                            "images":["me.png", "you.png"],
-                            "tags":["trees", "vegetation"]
-                           }
+        self.post_meetup1 = {"id": 1,
+                             "topic":"Miraa",
+                             "meetup_date":"30/01/1990",
+                             "location":"Meru",
+                             "images":["me.png", "you.png"],
+                             "tags":["trees", "vegetation"]
+                            }
+
+        self.post_meetup2 = {"id": 2,
+                             "topic":"Python",
+                             "meetup_date":"3/01/1991",
+                             "location":"Nyeri",
+                             "images":["them.png", "they.png"],
+                             "tags":["Snake", "Camel"]
+                            }
+
+        self.meetups = [{"id": 1,
+                         "location": "Meru",
+                         "meetup_date": "30/01/1990",
+                         "tags": ["trees", "vegetation"],
+                         "topic": "Miraa"},
+
+                        {"id": 2,
+                         "location": "Nyeri",
+                         "meetup_date": "3/01/1991",
+                         "tags": ["Snake", "Camel"],
+                         "topic": "Python"}]
+
 
 
 class TestMeetups(MeetupsBaseTest):
@@ -35,8 +57,22 @@ class TestMeetups(MeetupsBaseTest):
 
         """ Test that a user can enter meetup details and create a meetup"""
 
-        response = self.client.post("api/v1/meetups",data = json.dumps(self.post_meetup), content_type = "application/json")
+        response = self.client.post("api/v1/meetups", data = json.dumps(self.post_meetup1), content_type = "application/json")
         result = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 201)
         self.assertEqual(result["status"], 201)
-        self.assertEqual(result["data"], [{"location": "Meru","meetup_date": "30/01/1990","tags": ["trees","vegetation"],"topic": "Miraa"}])
+        self.assertEqual(result["data"], [{"location": "Meru", "meetup_date": "30/01/1990", "tags": ["trees", "vegetation"], "topic": "Miraa"}])
+
+    def test_user_can_get_all_meetups(self):
+        """
+        Tests to show that a user can successfully get all meetups
+        """
+        self.client.post("api/v1/meetups", data= json.dumps(self.post_meetup1), content_type = "application/json")
+        self.client.post("api/v1/meetups", data = json.dumps(self.post_meetup2),  content_type = "application/json")
+
+        response = self.client.get("api/v1/meetups", content_type = "application/json")
+        self.assertEqual(response.status_code, 200)
+
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(result["status"], 200)
+        self.assertEqual(result["data"], self.meetups)
