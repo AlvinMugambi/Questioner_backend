0ac669550206abe807164a9c4ac6f9f916ee0553
AlvinMugambi <ngurual123@gmail.com>
2019-01-08 15:59:29 +0300

    [feature #163033755]Adds the app configuration environment to the app

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
+
+import os
+
+from dotenv import load_dotenv
+
 from app import create_app
 
-
-app = create_app()
+APP_ENV = os.getenv("APP_ENV")
+app = create_app(APP_ENV)
 
 
 if __name__ == '__main__':
-    app.run(debug=True )
+    app.run(debug=True)
