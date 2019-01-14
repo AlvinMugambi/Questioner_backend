255bcae1c2fdd7df436bbdc0aa88176e81f6d93f
AlvinMugambi <ngurual123@gmail.com>
2019-01-14 23:37:47 +0300

    [chore #163205778]remove all version 2 contents from v1 develop

--- app/__init__.py
+++ app/__init__.py
@@ -8,10 +8,6 @@ from flask import Flask, Blueprint, jsonify
 from app.api.v1.views.meetup_views import version1 as meetups
 from app.api.v1.views.question_views import version1 as questions
 from app.api.v1.views.user_views import version1 as users
-from app.api.v2.views.meetup_views import version2 as meetups2
-# from app.api.v2.views.question_views import version2 as questions2
-from app.api.v2.views.user_views import version2 as users2
-from app.api.v2.models.database import init_db
 from config import app_config
 
 
@@ -23,10 +19,6 @@ def create_app(app_environment):
     app.register_blueprint(meetups)
     app.register_blueprint(questions)
     app.register_blueprint(users)
-    app.register_blueprint(meetups2)
-    # app.register_blueprint(questions2)
-    app.register_blueprint(users2)
-    init_db(app_config["db_url"])
 
     @app.errorhandler(404)
     def page_not_found(error):
