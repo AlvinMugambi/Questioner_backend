728a632bf37eaa95ae3b80268462527e5cb00828
AlvinMugambi <ngurual123@gmail.com>
2019-01-15 12:27:43 +0300

    [feature #163216581]add validators for string and int inputs

--- app/api/v1/utils/validators.py
+++ app/api/v1/utils/validators.py
@@ -21,26 +21,31 @@ def check_password(password, confirmed_password):
         # check if password meets required length
     if len(password) < 6 or len(password) > 12:
         abort(make_response(jsonify(
+            status=400,
             error="Password should not be less than 6 characters or exceed 12"), 400))
 
     # check if password contains at least an alphabet(a-z)
     if not re.search("[a-z]", password):
         abort(make_response(jsonify(
+            status=400,
             error="Password should contain a letter between a-z"), 400))
 
     # check if password contains at least an upper case letter
     if not re.search("[A-Z]", password):
         abort(make_response(jsonify(
+            status=400,
             error="Password should contain a capital letter"), 400))
 
     # check if password contains at least a number(0-9)
     if not re.search("[0-9]", password):
         abort(make_response(jsonify(
+            status=400,
             error="Password should contain a number(0-9)"), 400))
 
     # Checks if passwords provided by the users match
     if password != confirmed_password:
         abort(make_response(jsonify(
+            status=400,
             error="Passwords don't match!"), 400))
 
     # If they match..
@@ -57,11 +62,15 @@ def validate_email(email):
 
     for user in USERS:
         if email == user.email:
-            abort(make_response(jsonify(error="Email already taken!"), 400))
+            abort(make_response(jsonify(
+                status="409",
+                error="Email already taken!"), 409))
     try:
         user, domain = str(email).split("@")
     except ValueError:
-        abort(make_response(jsonify(error="Invalid Email"), 400))
+        abort(make_response(jsonify(
+            status=400,
+            error="Invalid Email"), 400))
     if not user or not domain:
         abort(make_response(jsonify(error="Invalid Email"), 400))
 
@@ -69,9 +78,13 @@ def validate_email(email):
     try:
         dom_1, dom_2 = domain.split(".")
     except ValueError:
-        abort(make_response(jsonify(error="Invalid Email"), 400))
+        abort(make_response(jsonify(
+            status=400,
+            error="Invalid Email"), 400))
     if not dom_1 or not dom_2:
-        abort(make_response(jsonify(error="Invalid Email"), 400))
+        abort(make_response(jsonify(
+            status=400,
+            error="Invalid Email"), 400))
 
     return email
 
@@ -87,6 +100,20 @@ def check_for_whitespace(data):
                 'error':'{} field cannot be left blank'.format(keys)}), 400))
 
     return True
+
+
+def check_if_string(firstname, lastname, username):
+    if not re.match("^[A-Za-z]*$", firstname):
+        abort(make_response(jsonify({
+            "status": 400, "Error":  "Make sure you only use letters in your firstname"}), 400))
+
+    if not re.match("^[A-Za-z]*$", lastname):
+        abort(make_response(jsonify({
+            "status": 400, "Error":  "Make sure you only use letters in your lastname"}), 400))
+
+    if not re.match("^[A-Za-z]*$", username):
+        abort(make_response(jsonify({
+            "status": 400, "Error":  "Make sure you only use letters in your username"}), 400))
 
 
 def query_db_wrong_password(username, password):
