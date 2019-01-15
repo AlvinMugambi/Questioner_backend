a5ebb89a2188b7b021a0e511d0201f86fd6c293a
AlvinMugambi <ngurual123@gmail.com>
2019-01-15 21:57:34 +0300

    [feature #163229435]add user input validation username, passwords

--- app/api/v2/utils/validators.py
+++ app/api/v2/utils/validators.py
@@ -19,23 +19,33 @@ def check_password(password, confirmed_password):
     """
         # check if password meets required length
     if len(password) < 6 or len(password) > 12:
-        abort(make_response(jsonify(error="Password should not be less than 6 characters or exceed 12"), 400))
+        abort(make_response(jsonify(
+            status=400,
+            error="Password should not be less than 6 characters or exceed 12"), 400))
 
     # check if password contains at least an alphabet(a-z)
     if not re.search("[a-z]", password):
-        abort(make_response(jsonify(error="Password should contain a letter between a-z"), 400))
+        abort(make_response(jsonify(
+            status=400,
+            error="Password should contain a letter between a-z"), 400))
 
     # check if password contains at least an upper case letter
     if not re.search("[A-Z]", password):
-        abort(make_response(jsonify(error="Password should contain a capital letter"), 400))
+        abort(make_response(jsonify(
+            status=400,
+            error="Password should contain a capital letter"), 400))
 
     # check if password contains at least a number(0-9)
     if not re.search("[0-9]", password):
-        abort(make_response(jsonify(error="Password should contain a number(0-9)"), 400))
+        abort(make_response(jsonify(
+            status=400,
+            error="Password should contain a number(0-9)"), 400))
 
     # Checks if passwords provided by the users match
     if password != confirmed_password:
-        abort(make_response(jsonify(error="Passwords don't match!"), 400))
+        abort(make_response(jsonify(
+            status=400,
+            error="Passwords don't match!"), 400))
 
     # If they match..
     hashed_password = generate_password_hash(password, method='sha256')
@@ -46,24 +56,79 @@ def check_password(password, confirmed_password):
 def validate_email(email):
     """
     The checker function for validating an Email
-     check if the email is a valid email and if it is already in use
+    if the email is a valid email and if it is already in use
     """
     try:
         user, domain = str(email).split("@")
+        if not re.match("^[A-Za-z]*$", user):
+            abort(make_response(jsonify({
+                "status": 400, "Error":  "Invalid Email"}), 400))
     except ValueError:
-        abort(make_response(jsonify(error="Invalid Email"), 400))
+        abort(make_response(jsonify(
+            status=400,
+            error="Invalid Email"), 400))
     if not user or not domain:
         abort(make_response(jsonify(error="Invalid Email"), 400))
 
     # Check that domain is valid
     try:
         dom_1, dom_2 = domain.split(".")
+        if not re.match("^[A-Za-z]*$", dom_1):
+            abort(make_response(jsonify({
+                "status": 400, "Error":  "Invalid Email"}), 400))
+        if not re.match("^[A-Za-z]*$", dom_2):
+            abort(make_response(jsonify({
+                "status": 400, "Error":  "Invalid Email"}), 400))
     except ValueError:
-        abort(make_response(jsonify(error="Invalid Email"), 400))
+        abort(make_response(jsonify(
+            status=400,
+            error="Invalid Email"), 400))
     if not dom_1 or not dom_2:
-        abort(make_response(jsonify(error="Invalid Email"), 400))
+        abort(make_response(jsonify(
+            status=400,
+            error="Invalid Email"), 400))
 
     return email
+
+def check_for_whitespace(data):
+    """
+    Check for whitespace only in input data
+    """
+    for keys, value in data.items():
+        if not value.strip():
+            abort(make_response(jsonify({
+                'status': 400,
+                'error':'{} field cannot be left blank'.format(keys)}), 400))
+
+    return True
+
+def check_if_string(data):
+    if not re.match("^[A-Za-z]*$", data['firstname']):
+        abort(make_response(jsonify({
+            "status": 400,
+            "Error":  "Make sure you only use letters in your firstname"}), 400))
+
+    if not re.match("^[A-Za-z]*$", data['lastname']):
+        abort(make_response(jsonify({
+            "status": 400, "Error":
+            "Make sure you only use letters in your lastname"}), 400))
+
+    if not re.match("^[A-Za-z]*$", data['username']):
+        abort(make_response(jsonify({
+            "status": 400,
+            "Error":  "Make sure you only use letters in your username"}), 400))
+
+
+def check_phone_number(phone):
+    if not re.match('^[0-9]*$', phone):
+        abort(make_response(jsonify({
+            "status": 400,
+            "Error":  "Phone number should be integers only"}), 400))
+
+    if len(phone) < 10 or len(phone) > 10:
+        abort(make_response(jsonify({
+            "status": 400,
+            "Error":  "Phone number should be 10 digits."}), 400))
 
 
 def check_if_admin():
